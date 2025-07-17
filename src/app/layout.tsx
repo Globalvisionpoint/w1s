47b65ddb16d452717d171356beaf05397e9d4011
Globalvisionpoint <globalvisionpoint@gmail.com>
2025-07-17 11:45:16 +0300

    Fonturi self-hosted: Inter 300/500/600/700 și Space Grotesk 500/600/700, eliminare Google Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,10 +33,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@500;700&display=swap"
-          rel="stylesheet"
-        />
+        {/* Elimin Google Fonts, folosim doar self-hosted */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify({
